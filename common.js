e24a6a7aa6dac6cd76b635c7ba7a2167ad1dc83b
Keishake <keishake0925@gmail.com>
2015-07-30 14:03:01 +0900

    read secrets from ENV

--- common.js
+++ common.js
@@ -1,6 +1,19 @@
-var env = require('./env.json');
+if(process.env.NODE_ENV === 'heroku'){
+  exports.config = function() {
+    config = {
+      "consumer_key" : process.env.CONSUMER_KEY,
+      "consumer_secret" : process.env.CONSUMER_SECRET,
+      "access_token_key" : process.env.ACCESS_TOKEN_KEY,
+      "access_token_secret" : process.env.ACCESS_TOKEN_SECRET,
+      "slack_webhook_url" : process.env.WEBHOOK_URL
+    }
+    return config;
+  };
 
-exports.config = function() {
-  var node_env = process.env.NODE_ENV || 'development';
-  return env[node_env];
-};
+}else{
+  var env = require('./env.json');
+  exports.config = function() {
+    return env;
+  };
+}
+
